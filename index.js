a201aa009dcf9dc308f3198180d9e609e056c3a1
DESKTOP-TG45HMC\DarkTuan <bachdangtuan.dev@gmail.com>
2024-04-03 21:31:31 +0700

    cập nhật lại productions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path')
 const port = 3000;
-const mongoURI = 'mongodb+srv://dangtuan:0934010704@url-short.nd90j5s.mongodb.net/?retryWrites=true&w=majority'
+// const mongoURI = 'mongodb+srv://dangtuan:0934010704@url-short.nd90j5s.mongodb.net/?retryWrites=true&w=majority'
 const app = express()
 const mongoose = require('mongoose')
 const { rootRouter } = require("./routes/routers");
@@ -12,10 +12,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.json())
 
+
+
 // KẾT NỐI DATABASE MONGO
+const dbName = 'production-short-url';
+const username = 'darktuan';
+const password = 'dangtuan12';
+const ip = '10.0.0.223';
+const portMongo = '32680';
+const connectionString = `mongodb://${username}:${password}@${ip}:${portMongo}/${dbName}`;
+
+
 // connect DB
 mongoose.set('strictQuery', true);
-mongoose.connect(mongoURI, {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
